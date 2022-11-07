5c885a345656e8bda89a855b575e687dca81f925
pers5not <pers5not@gmail.com>
2022-11-07 16:43:51 +0200

    Text generator

--- text_gen.py
+++ text_gen.py
@@ -5,4 +5,4 @@ model = pipeline('text-generation')
 sentence = model("Hello Microwave", do_sample=True, top_k=50, temperature=0.9, max_length=100, num_return_sequences=2)
 
 for _ in sentence:
-    print(_['generated_text'])
+    print(_)
